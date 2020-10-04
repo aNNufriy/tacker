506df8a94fc5036c8f28c0484db62ed542d298b2
Alexander Schedrov <schedrow@gmail.com>
2020-10-04 08:15:03 +0300

    removed unused imports for code quality test

--- src/test/java/ru/testfield/tacker/TackerTest.java
+++ src/test/java/ru/testfield/tacker/TackerTest.java
@@ -2,7 +2,7 @@ package ru.testfield.tacker;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TackerTest {
 
