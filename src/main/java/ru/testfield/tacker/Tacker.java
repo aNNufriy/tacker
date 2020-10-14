c9c0192b99d985107d186e6c9285675eb5613bfb
Alexander Schedrov <schedrow@gmail.com>
2020-10-14 12:00:39 +0300

    getPack logic added

--- src/main/java/ru/testfield/tacker/Tacker.java
+++ src/main/java/ru/testfield/tacker/Tacker.java
@@ -1,7 +1,43 @@
 package ru.testfield.tacker;
 
-public class Tacker {
-    public String helloWorld(){
-        return "hello, world";
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Tacker<T> {
+
+    private final Lock retrieveLock = new ReentrantLock();
+
+    private final BlockingQueue<T> blockingQueue;
+
+    public Tacker(BlockingQueue<T> blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    public void addValue(T value) {
+        try {
+            blockingQueue.put(value);
+        } catch (InterruptedException e) {
+            System.out.println("Unable to add value to blocking queue: interrupted");
+        }
+    }
+
+    public List<T> getPack() {
+        List<T> pack = new ArrayList<>(blockingQueue.size());
+        retrieveLock.lock();
+        try {
+            while(blockingQueue.size()>0){
+                try {
+                    pack.add(blockingQueue.take());
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }finally {
+            retrieveLock.unlock();
+        }
+        return pack;
     }
 }
--- src/test/java/ru/testfield/tacker/TackerTest.java
+++ src/test/java/ru/testfield/tacker/TackerTest.java
@@ -2,14 +2,28 @@ package ru.testfield.tacker;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TackerTest {
 
     @Test
-    void helloWorldTest(){
-        Tacker tacker = new Tacker();
-        assertEquals("hello, world", tacker.helloWorld());
+    void addTaskTest() throws InterruptedException, ExecutionException {
+        int capacity = 10000;
+        Tacker<String> tacker = new Tacker<>(new LinkedBlockingDeque<>(capacity));
+        Runnable callableTask = ()->tacker.addValue(UUID.randomUUID().toString());
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        List<Future<?>> futures = new ArrayList<>();
+        for(int i=0; i<capacity; i++) {
+            futures.add(executorService.submit(callableTask));
+        }
+        for(Future<?> future: futures){
+            future.get();
+        }
+        assertEquals(capacity, tacker.getPack().size());
     }
-
 }
