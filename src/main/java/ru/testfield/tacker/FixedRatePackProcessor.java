11788d640ec2c2478315f3febc075642f3578690
Alexander Schedrov <schedrow@gmail.com>
2020-10-19 14:30:27 +0300

    fixed codacy issues

--- src/main/java/ru/testfield/tacker/FixedRatePackProcessor.java
+++ src/main/java/ru/testfield/tacker/FixedRatePackProcessor.java
@@ -9,17 +9,28 @@ public class FixedRatePackProcessor<T> extends PackProcessor<T> {
 
     private final ScheduledExecutorService scheduledExecutorService;
     private final Consumer<T> consumer;
+    private final long rateMillis;
 
     public FixedRatePackProcessor(Tacker<T> tacker, long rateMillis, Consumer<T> consumer) {
         super(tacker);
-        this.scheduledExecutorService = this.createExecutor();
         this.consumer = consumer;
+        this.rateMillis = rateMillis;
+        this.scheduledExecutorService = this.createExecutor();
         this.scheduledExecutorService.scheduleAtFixedRate(this::processPack,0, rateMillis, TimeUnit.MILLISECONDS);
     }
 
     protected ScheduledExecutorService createExecutor() {
         int parallelism = 10 * Runtime.getRuntime().availableProcessors();
         return Executors.newScheduledThreadPool(parallelism);
+    }
+
+    public void stopProcessing(){
+        this.scheduledExecutorService.shutdown();
+        try {
+            this.scheduledExecutorService.awaitTermination(10*this.rateMillis, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/test/java/ru/testfield/tacker/TackerTest.java
+++ src/test/java/ru/testfield/tacker/TackerTest.java
@@ -2,25 +2,23 @@ package ru.testfield.tacker;
 
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.function.Consumer;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TackerTest {
 
     @Test
     void addTaskGetPackTest() throws InterruptedException, ExecutionException {
-        Tacker<String> tacker = new Tacker<>(new LinkedBlockingDeque<>());
-        List<Future<?>> futures = addRandomTasks(tacker, 10000);
+        int capacity = 10000;
+        Tacker<String> tacker = new Tacker<>(new LinkedBlockingDeque<>(capacity));
+        List<Future<?>> futures = addRandomTasks(tacker, capacity);
+        int counter = 0;
         for(Future<?> future: futures){
+            counter++;
             future.get();
         }
-
-        Consumer<String> consumer = System.out::println;
-
-        new FixedRatePackProcessor<>(tacker,500, consumer);
-
-        Thread.sleep(10000);
+        assertEquals(capacity,counter);
     }
 
     private List<Future<?>> addRandomTasks(Tacker<String> tacker, int capacity) {
